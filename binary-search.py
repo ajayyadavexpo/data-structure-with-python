6cc6d0a2159c1872006ba78ebadd98c4c003fd3b
AjayYadavAi <proajayyadav0525@gmail.com>
2020-01-25 20:23:30 +0530

    added new file binary-search

---
+++ binary-search.py
@@ -0,0 +1,20 @@
+def binarySearch(array,left,right,search):
+	if(left <= right):
+		mid = left+(right-1)//2
+
+		if(array[mid] == search):
+			return mid
+		elif(array[mid] > search):
+			return binarySearch(array,left,mid-1,search)
+		else:
+			return binarySearch(array,mid+1,right,search)
+	else:
+		return -1
+
+array = [3,5,7,20,22]
+search= 20
+result = binarySearch(array,0,len(array)-1,search)
+if(result == -1):
+	print('Element is not present in array')
+else:
+	print("Element is present at index : ",result)
--- linear-search.py
+++ linear-search.py
@@ -6,9 +6,9 @@ def linear_search(array,len,search):
 	return -1
 
 array = [2,4,6,3,90,7]
-x = 90
+search = 90
 n = len(array)
-result = linear_search(array,n,x)
+result = linear_search(array,n,search)
 if(result == -1):
 	print("Element is not present in the Array")
 else:
