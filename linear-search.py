2855874f3fb7a71f1a2a49c896668f78758af141
AjayYadavAi <proajayyadav0525@gmail.com>
2020-01-25 20:12:02 +0530

    added linear-search

---
+++ linear-search.py
@@ -0,0 +1,15 @@
+def linear_search(array,len,search):
+	for index in range(0,len):
+		if(array[index] == search):
+			return index
+
+	return -1
+
+array = [2,4,6,3,90,7]
+x = 90
+n = len(array)
+result = linear_search(array,n,x)
+if(result == -1):
+	print("Element is not present in the Array")
+else:
+	print("Element is present at index : ",result)
