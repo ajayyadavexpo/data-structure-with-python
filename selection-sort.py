ee3424778b01b0b978a275260a92a7d6242f4fe0
AjayYadavAi <proajayyadav0525@gmail.com>
2020-01-26 15:49:37 +0530

    added selection -sort

---
+++ selection-sort.py
@@ -0,0 +1,14 @@
+def selectionSort(array):
+	size = len(array)
+	for step in range(size):
+		min_index = step
+		for element in range(min_index+1,size):
+			if(array[element] < array[min_index]):
+				min_index = element
+
+		(array[step],array[min_index]) = (array[min_index],array[step])
+
+
+data = [4,2,5,6,3]
+selectionSort(data)
+print("Sorted Data with selection sort : ",data)
