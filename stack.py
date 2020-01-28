be82f1cbc2f9c972056c663728d9e4c59c5610c5
AjayYadavAi <proajayyadav0525@gmail.com>
2020-01-28 20:45:20 +0530

    changes in stack

--- stack.py
+++ stack.py
@@ -1,3 +1,15 @@
+# The functions associated with stack are:
+
+
+
+# empty() – Returns whether the stack is empty – Time Complexity : O(1)
+# size() – Returns the size of the stack – Time Complexity : O(1)
+# top() – Returns a reference to the top most element of the stack – Time Complexity : O(1)
+# push(g) – Adds the element ‘g’ at the top of the stack – Time Complexity : O(1)
+# pop() – Deletes the top most element of the stack – Time Complexity : O(1)
+
+
+
 stack = [] 
   
 stack.append('a') 
